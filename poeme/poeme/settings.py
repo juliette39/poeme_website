e229d77842a3aa190de42a3fe418a80755662ca4
Juliette DEBONO <juliette.debono@telecom-sudparis.eu>
2023-05-09 05:11:47 +0200

    base de données

--- poeme/poeme/settings.py
+++ poeme/poeme/settings.py
@@ -77,7 +77,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'mots': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'generator/PoemeDB.sqlite3',
+    },
 }
 
 
